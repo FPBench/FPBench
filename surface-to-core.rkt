9ec4d8df22f6b015a3c62878addeb86e13109689
Pavel Panchekha <me@pavpanchekha.com>
2016-04-14 15:30:19 -0700

    Much better compiler of surface syntax

--- surface-to-core.rkt
+++ surface-to-core.rkt
@@ -1,20 +1,25 @@
 #lang racket
 
+(define (attribute? symb)
+  (and (symbol? symb) (string-prefix? (symbol->string symb) ":")))
+
 (define (canonicalize body)
+  (pretty-print body)
   (match body
     [(or 'E 'PI) body]
     [`(+ ,branch) (canonicalize branch)]
-    [`(+ ,args ... ,arg) `(+ ,(canonicalize (cons '+ args)) ,arg)]
+    [`(+ ,args ... ,arg) `(+ ,(canonicalize (cons '+ args)) ,(canonicalize arg))]
     [`(* ,branch) (canonicalize branch)]
-    [`(* ,args ... ,arg) `(* ,(canonicalize (cons '* args)) ,arg)]
+    [`(* ,args ... ,arg) `(* ,(canonicalize (cons '* args)) ,(canonicalize arg))]
     [`(- ,head) (canonicalize head)]
     [`(- ,head ,arg) `(- ,(canonicalize head) ,(canonicalize arg))]
-    [`(- ,args ... ,tail) `(- ,(canonicalize (cons '- args)) ,tail)]
+    [`(- ,args ... ,tail) `(- ,(canonicalize (cons '- args)) ,(canonicalize tail))]
     [`(/ ,head ,arg) `(/ ,(canonicalize head) ,(canonicalize arg))]
-    [`(/ ,args ... ,tail) `(/ ,(canonicalize (cons '/ args)) ,tail)]
-    [`(,(and (or '< '> '<= '>= '==) op) ,args ...)
-     `(and ,(for/list ([fst args] [snd (cdr args)])
-              `(,op ,fst ,snd)))]
+    [`(/ ,args ... ,tail) `(/ ,(canonicalize (cons '/ args)) ,(canonicalize tail))]
+    [(list (and (or '< '> '<= '>= '==) op) fst snd)
+     `(,op ,fst ,snd)]
+    [(list (and (or '< '> '<= '>= '==) op) args ...)
+     `(and ,@(for/list ([fst args] [snd (cdr args)]) `(,op ,fst ,snd)))]
     [(? list?) (cons (car body) (map canonicalize (cdr body)))]
     [(? symbol?) body]
     [(? number?) body]))
@@ -22,7 +27,11 @@
 (define (substitute body bindings)
   (match body
     [(or 'E 'PI) body]
-    [(? (λ (x) (assoc x bindings))) (cdr (assoc body bindings))]
+    [(? (λ (x) (assoc x bindings)))
+     ;; The (if) handles the case when we don't have a value bound for the variable
+     (if (null? (cdr (assoc body bindings)))
+         body
+         (cdr (assoc body bindings)))]
     [`(,op ,args ...) (cons op (map (curryr substitute bindings) args))]
     [(? symbol?) body]
     [(? number?) body]))
@@ -39,59 +48,92 @@
   (let loop ([bindings (reverse bindings)] [clean '()])
     (match bindings
       [(cons (cons var value) bindings*)
-       (if (or (ormap (curryr appears? var) bindings*)
-               (appears? (cons 'self value) var))
+       (if (and (not (null? value))
+                (or (ormap (curryr appears? var) bindings*)
+                    (appears? (cons 'self value) var)))
            (cons (cons var value) (loop bindings* clean))
-           (cons (cons var 'UNUSED) (loop bindings* clean)))]
+           (cons (list var) (loop bindings* clean)))]
       ['() clean])))
 
 ;; Bindings store variable-value bindings in an alist.
 
-(define (compile-statements statements)
-  (let loop ([bindings '()] [statements statements])
-    (if (null? statements)
-        (values 'UNUSED (clean-unused bindings))
-        (match (car statements)
-          [`[= ,(? symbol? var) ,value]
-           (loop (cons (cons var (substitute (canonicalize value) bindings))
-                       bindings)
-                 (cdr statements))]
-          [`(output ,vals ...)
-           (values
-            (canonicalize
-             (cons '+
-                   (map (λ (value) (substitute (canonicalize value) bindings))
-                        vals)))
-            bindings)]
-          [`(while ,cond ,substatements ...)
-           (define-values (outexpr outb) (compile-statements substatements))
-           (when (not (equal? outexpr 'UNUSED))
-             (error "(while) loop cannot contain (output) expression."))
-           (define loopvars
-             (for/list ([bind outb] #:when (not (equal? (cdr bind) 'UNUSED)))
-               (match (assoc (car bind) bindings)
-                 [(cons var init)
-                  `[,var ,init ,(cdr bind)]]
-                 [#f
-                  `[,(car bind) UNUSED ,(cdr bind)]])))
-           (define-values (out bindings*)
-             (loop (filter (λ (x) (not (assoc (car x) outb))) bindings)
-                   (cdr statements)))
-           (values
-            `(while ,(canonicalize cond)
-               ,loopvars
-               ,out)
-            bindings*)]
-          [`(if cond (then ,iftstatements ...))
-           '???]
-          [`(if cond (then ,iftstatements ...) (else ,iffstatements ...))
-           '???]))))
+(define (assigned statements)
+  (match statements
+    ['() '()]
+    [(list `[= ,(? symbol? var) ,_] rest ...)
+     (cons var (assigned rest))]
+    [(list `(while ,_ ,sub ...) rest ...)
+     (append (assigned sub) (assigned rest))]))
+
+(define (compile-statements statements [bindings '((E) (PI))] [constants '()])
+  (match statements
+    ['() (values 'UNUSED (reverse bindings) constants)]
+    [(list `(output ,vals ...))
+     (values
+      (canonicalize
+       (cons '+ (map (λ (value) (substitute (canonicalize value) bindings)) vals)))
+      bindings
+      constants)]
+    [(list `[= ,(? symbol? var) ,(? number? value)] rest ...)
+     (compile-statements rest (cons (list var) bindings) (cons (cons var value) constants))]
+    [(list `[= ,(? symbol? var) ,value] rest ...)
+     (define value* (substitute (canonicalize value) bindings))
+     (compile-statements rest (cons (cons var value*) bindings) constants)]
+    [(list `(while ,cond ,substatements ...) rest ...)
+     (define trashed (assigned substatements))
+     (define-values (notc truec)
+       (partition (λ (x) (member (car x) trashed)) constants))
+     (define subb
+       (append (map list trashed) bindings))
+
+     (define-values (outexpr outb outc)
+       (compile-statements substatements subb truec))
+
+     (when (not (equal? outexpr 'UNUSED))
+       (error "(while) loop cannot contain (output) expression."))
+     
+     (define binds-extension (drop outb (length subb)))
+
+     (define binds-extension*
+       (for/list ([bind binds-extension])
+         (if (ormap (compose (curryr appears? (car bind)) cdr) binds-extension)
+             bind
+             (list (car bind)))))
+
+     (define loopvars
+       (for/list ([bind binds-extension*] #:when (not (null? (cdr bind))))
+         (define init-rec (assoc (car bind) (append notc bindings)))
+         (define init (if (null? (cdr init-rec)) (car bind) (cdr init-rec)))
+         `[,(car bind) ,init ,(cdr bind)]))
+
+     (define newb
+       (append (map list trashed) subb))
+
+     (define-values (out endb endc)
+       (compile-statements rest newb outc))
+
+     (values
+      `(while ,(canonicalize cond) ,loopvars ,out)
+      endb
+      endc)]))
 
 (define (compile-program body)
-  (match-define `(function ,statements ...) body)
-  (define-values (out bs) (compile-statements statements))
-  `(lambda (,@(for/list ([b bs] #:when (number? (cdr b)))
-                `[,(car b) ,(cdr b)]))
+  (match-define `(function ,lines ...) body)
+
+  (define-values (attributes statements)
+    (let loop ([attrs '()] [stats '()] [lines lines])
+      (cond
+       [(null? lines) (values (reverse attrs) (reverse stats))]
+       [(equal? (car lines) ':pre)
+        (loop (cons (cons (car lines) (canonicalize (cadr lines))) attrs) stats (cddr lines))]
+       [(attribute? (car lines))
+        (loop (cons (cons (car lines) (cadr lines)) attrs) stats (cddr lines))]
+       [else
+        (loop attrs (cons (car lines) stats) (cdr lines))])))
+
+  (define-values (out bs cs) (compile-statements statements))
+  `(lambda (,@(for/list ([c cs]) `[,(car c) ,(cdr c)]))
+     ,@(apply append (for/list ([pair attributes]) (list (car pair) (cdr pair))))
      ,out))
 
 (module+ main

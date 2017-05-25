7e6a8446a20253a15146e9d582adb5a573860f74
Pavel Panchekha <me@pavpanchekha.com>
2017-05-24 18:23:32 -0700

    Massive overhaul of core2c
    The FPCore to C compiler should now output equivalent C code, though the
    equivalence has yet to be tested. Unlike the previous version, this
    correctly handles let in expression contexts.

--- tools/core2c.rkt
+++ tools/core2c.rkt
@@ -2,20 +2,13 @@
 
 (require "common.rkt")
 
-(define (attribute? symb)
-  (and (symbol? symb) (string-prefix? (symbol->string symb) ":")))
-
-(define (canonicalize-program prog)
-  (match-define (list 'lambda (list args ...) props&body ...) prog)
-  (let loop ([props&body props&body] [props '()])
-    (match props&body
-      [(list (? attribute? propname) propval rest ...)
-       (loop rest (cons (cons propname propval) props))]
-      [(list body rest ...)
-       (values args body (append (reverse props) rest))])))
-
 (define (fix-name name)
-  (string-replace (~a name) #rx"[^a-zA-Z0-9]" "_"))
+  (string-join
+   (for/list ([char (~a name)])
+     (if (regexp-match #rx"[a-zA-Z0-9_]" (string char))
+         (string char)
+         (format "$~a" (char->integer char))))
+   ""))
 
 (define/match (application->c type . args)
   [(type (list '+ a b))     (format "(~a + ~a)" a b)]
@@ -38,35 +31,88 @@
   [(type (list (and (or 'sqrt 'hypot 'exp 'expm1 'pow 'log 'log1p 'sin 'cos 'tan 'asin 'acos 'atan 'sinh 'cosh 'tanh 'atan2) f) args ...))
    (format "~a~a(~a)" f (type->suffix type) (string-join args ", "))])
 
-(define/match (typeof expr #:fptype [fptype 'binary64])
-  [((list (or '> '< '>= '<= 'and 'or) _ ...) ftype) 'bool]
-  [(_ fptype) (type->c fptype)])
-
 (define/match (type->c type)
-  [('binary64) 'double]
-  [('binary32) 'float]
-  [('binary80) '|long double|])
+  [('binary64) "double"]
+  [('binary32) "float"]
+  [('binary80) "long double"])
 
 (define/match (type->suffix type)
   [('binary64) ""]
   [('binary32) "f"]
   [('binary80) "l"])
 
-(define/match (value->c expr #:type [type 'binary64])
-  [('E _) "exp(1.0)"]
-  [('PI _) "atan2(1.0, 0.0)"]
-  [((? symbol?) _) (fix-name expr)]
-  [((? number?) 'binary32) (format "~af" (real->double-flonum expr))]
-  [((? number?) 'binary64) (format "~a" (real->double-flonum expr))])
+(define *names* (make-parameter (make-hash)))
 
-(define/match (if->c expr #:type [type 'binary64])
-  [(`(if ,cond ,ift ,iff) _)
-   (format "(~a ? ~a : ~a)" (expr->c cond #:type type) (expr->c ift #:type type) (expr->c iff #:type type))])
+(define (gensym name)
+  (define prefixed
+    (filter (λ (x) (string-prefix? (~a x) (~a name))) (dict-keys (*names*))))
+  (define options
+    (cons name (for/list ([_ prefixed] [i (in-naturals)]) (string->symbol (format "~a_~a" name (+ i 1))))))
+  (define name*
+    (car (set-subtract options prefixed)))
+  (dict-set! (*names*) name* #t)
+  name*)
 
-(define/match (expr->c expr #:type [type 'binary64])
-  [(`(if ,cond ,ift ,iff) _) (if->c expr #:type type)]
-  [((? list?) _) (apply application->c type (car expr) (map (λ (x) (expr->c x #:type type)) (cdr expr)))]
-  [(_ _) (value->c expr #:type type)])
+(define/match (expr->c expr #:names [names #hash()] #:to [destination #f] #:type [type 'binary64] #:indent [indent "\t"])
+  ;; Takes in an expression. Returns an expression and a new set of names
+  [(`(let ([,vars ,vals] ...) ,body) _ _ _ _)
+   (define vars* (map gensym vars))
+   (for ([var vars] [var* vars*] [val vals])
+     (printf "~a~a ~a;\n" indent (type->c type) (fix-name var*))
+     (expr->c val #:names names #:to var* #:type type #:indent indent))
+   (define names*
+     (for/fold ([names* names]) ([var vars] [var* vars*])
+       (dict-set names* var var*)))
+   (expr->c body #:names names* #:to destination #:type type #:indent indent)]
+  [(`(if ,cond ,ift ,iff) _ _ _ _)
+   (define test (expr->c cond #:names names #:to #f #:type type #:indent indent))
+   (define outvar (or destination (gensym 'temp)))
+   (unless destination ; otherwise already defined
+     (printf "~a~a ~a;\n" indent (type->c type) (fix-name outvar)))
+   (printf "~aif (~a) {\n" indent test)
+   (expr->c ift #:names names #:to outvar #:type type #:indent (format "~a\t" indent))
+   (printf "~a} else {\n" indent)
+   (expr->c iff #:names names #:to outvar #:type type #:indent (format "~a\t" indent))
+   (printf "~a}\n" indent)
+   (fix-name outvar)]
+  [(`(while ,cond ([,vars ,inits ,updates] ...) ,retexpr) _ _ _ _)
+   (define vars* (map gensym vars))
+   (for ([var vars] [var* vars*] [val inits])
+     (printf "~a~a ~a;\n" indent (type->c type) (fix-name var*))
+     (expr->c val #:names names #:to var* #:type type #:indent indent))
+   (define names*
+     (for/fold ([names* names]) ([var vars] [var* vars*])
+       (dict-set names* var var*)))
+   (define test-var (gensym 'test))
+   (printf "~aint ~a;\n" indent (fix-name test-var))
+   (expr->c cond #:names names* #:to test-var #:type type #:indent indent)
+   (printf "~awhile (~a) {\n" indent test-var)
+   (define temp-vars (map gensym vars))
+   (for ([temp-var temp-vars] [update updates])
+     (printf "~a\t~a ~a;\n" indent (type->c type) (fix-name temp-var))
+     (expr->c update #:names names* #:to temp-var #:type type #:indent (format "~a\t" indent)))
+   (for ([var* vars*] [temp-var temp-vars])
+     (printf "~a\t~a = ~a;\n" indent (fix-name var*) (fix-name temp-var)))
+   (expr->c cond #:names names* #:to test-var #:type type #:indent (format "~a\t" indent))
+   (printf "~a}\n" indent)
+   (expr->c retexpr #:names names* #:to destination #:type type #:indent indent)]
+  [((list operator args ...) _ _ _ _)
+   (define args_c
+     (map (λ (arg) (expr->c arg #:names names #:to #f #:type type #:indent indent)) args))
+   (define out (apply application->c type operator args_c))
+   (when destination
+     (printf "~a~a = ~a;\n" indent (fix-name destination) out))
+   out]
+  [((? symbol?) _ _ _ _)
+   (define out (fix-name (dict-ref names expr expr)))
+   (when destination
+     (printf "~a~a = ~a;\n" indent (fix-name destination) out))
+   out]
+  [((? number?) _ _ _ _)
+   (define out (format "~a~a" (real->double-flonum expr) (type->suffix type)))
+   (when destination
+     (printf "~a~a = ~a;\n" indent (fix-name destination) out))
+   out])
 
 (define (function->c args body #:type [type 'binary64] #:name [name 'f])
   (define arg-strings
@@ -74,28 +120,14 @@
       (format "~a ~a" (type->c type) (fix-name (if (list? var) (car var) var)))))
   (format "~a ~a(~a) {\n~a}\n"
           (type->c type) name (string-join arg-strings ", ")
-          (with-output-to-string (λ () (program->c body #:type type)))))
-
-(define (program->c body #:type [type 'binary64])
-  (match body
-    [`(let ([,vars ,vals] ...) ,retexpr)
-     (for ([var vars] [val vals])
-       (printf "\t~a next_~a = ~a;\n" (typeof val #:fptype type) var (expr->c val #:type type)))
-     (for ([var vars] [val vals])
-       (printf "\t~a ~a = next_~a;\n" (typeof val #:fptype type) var var))
-     (program->c retexpr #:type type)]
-    [`(while ,cond ([,vars ,inits ,updates] ...) ,retexpr)
-     (for ([var vars] [init inits])
-       (printf "\t~a ~a = ~a;\n" (typeof init #:fptype type) var (expr->c init #:type type)))
-     (printf "\twhile (~a) {\n" (expr->c cond #:type type))
-     (for ([var vars] [update updates])
-       (printf "\t\t~a next_~a = ~a;\n" (typeof update #:fptype type) var (expr->c update #:type type)))
-     (for ([var vars])
-       (printf "\t\t~a = next_~a;\n" var var))
-     (printf "\t}\n")
-     (program->c retexpr #:type type)]
-    [_
-     (printf "\treturn ~a;\n" (expr->c body #:type type))]))
+          (with-output-to-string
+            (λ ()
+              (define hash (make-hash))
+              (for ([var args]) (dict-set! hash var #t))
+              (define out 
+                (parameterize ([*names* hash])
+                  (expr->c body #:type type)))
+              (printf "\treturn ~a;\n" out)))))
 
 (define (compile-program prog #:name name)
   (match-define (list 'FPCore (list args ...) props ... body) prog)

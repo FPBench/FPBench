a8765edbea52980edf2a8412b3172df741b580cc
Pavel Panchekha <me@pavpanchekha.com>
2018-01-07 15:30:32 -0800

    Skip more bad rows

--- infra/graphs.sh
+++ infra/graphs.sh
@@ -47,7 +47,7 @@ function clean_csv {
   local data="$1"
 
   awk -F',' "
-    NR == 1 || NF == $NCOLS && \$$DAISY_RES_ERR ~ /\./ {
+    NR == 1 || NF == $NCOLS && \$1 !~ /\"/ && \$3 !~ /\"/ && \$4 !~ /\"/ && \$5 !~ /\"/ && \$6 !~ /\"/ && \$7 !~ /\"/ && \$8 !~ /\"/ && \$9 !~ /\"/ {
       print \$0;
       next
     }

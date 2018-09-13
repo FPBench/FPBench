425c6ccb4a65d726f0a8eb4aebcbd92f1b8932fe
Pavel Panchekha <me@pavpanchekha.com>
2018-09-13 10:40:49 -0700

    Update filter script to look at all annotations

--- tools/filter.rkt
+++ tools/filter.rkt
@@ -16,29 +16,60 @@
      (cons 'let (append (append-map operators-in vals) (operators-in body)))]
     [`(if ,cond ,ift ,iff)
      (cons 'if (append (operators-in cond) (operators-in ift) (operators-in iff)))]
+    [`(! ,props ... ,body)
+     (operators-in body)]
     [(list op args ...) (cons op (append-map operators-in args))]
     [(? symbol?) '()]
     [(? number?) '()]))
 
+(define property-hash? (hash/c symbol? (set/c any/c)))
+(define (property-hash-add! hash props)
+  (define-values (_ properties) (parse-properties props))
+  (for ([(k v) (in-dict properties)])
+    (hash-update! hash k (curryr set-add v) (set))))
+
+(define/contract (property-values expr)
+  (-> expr? property-hash?)
+
+  (define out (make-hash))
+
+  (let loop ([expr expr])
+    (match expr
+      [`(while ,test ([,vars ,inits ,updates] ...) ,res)
+       (loop test) (for-each loop inits) (for-each loop updates) (loop res)]
+      [`(let ([,vars ,vals] ...) ,body)
+       (for-each loop vals) (loop body)]
+      [`(if ,cond ,ift ,iff)
+       (loop cond) (loop ift) (loop iff)]
+      [`(! ,props ... ,body)
+       (property-hash-add! out props)
+       (loop body)]
+      [(list op args ...) (for-each loop args)]
+      [(? symbol?) (void)]
+      [(? number?) (void)]))
+
+  out)
+
 (define/contract ((filter type values) prog)
   (-> symbol? (listof string?) (-> fpcore? boolean?))
   (match-define (list 'FPCore (list args ...) props ... body) prog)
-  (define-values (_ properties) (parse-properties props))
+  (define prop-hash (property-values body))
+  (property-hash-add! prop-hash props)
 
   (match* (type values)
-    [('cites (list value))
-     (set-member? (dict-ref properties ':cite '()) (string->symbol value))]
     [((or 'operator 'operation) (list value))
      (set-member? (operators-in body) (string->symbol value))]
     [((or 'operators 'operations) (list ops ...))
      (subset? (operators-in body) (map string->symbol ops))]
-    [((? symbol?) (list value))
-     (define prop (string->symbol (format ":~a" type)))
-     (and (dict-has-key? properties prop)
-          (equal? (~a (dict-ref properties prop)) value))]
     [((? symbol?) (list))
      (define prop (string->symbol (format ":~a" type)))
-     (dict-has-key? properties prop)]
+     (dict-has-key? prop-hash prop)]
+    [((? symbol?) (list values ...))
+     (define prop (string->symbol (format ":~a" type)))
+     (and (dict-has-key? prop-hash prop)
+          (subset? (map ~a (set->list (dict-ref prop-hash prop))) values))]
+    [('cites (list values ...))
+     (subset? (map string->symbol values) (append-map set->list (dict-ref prop-hash ':cite '())))]
     [(_ _)
      (raise-user-error 'filter "Unknown filter ~a with ~a arguments" type (length values))]))
 

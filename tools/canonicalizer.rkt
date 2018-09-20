a18a9949c0a112c01cdee3a8382cde1dc37559fa
DavidThien <dthien@cs.washington.edu>
2018-09-20 12:17:53 -0700

    Added canonicalize and propagate flags to the canonicalizer

--- tools/canonicalizer.rkt
+++ tools/canonicalizer.rkt
@@ -4,6 +4,9 @@
 (require racket/hash)
 (provide compile-program expr->canon)
 
+(define *to-canonicalize* (make-parameter '(:pre :spec)))
+(define *to-propagate* (make-parameter '(:precision :round :math-library)))
+
 (define (flatten-context ctx)
   (apply append (map (match-lambda [(cons prop val) (list prop val)]) (hash->list ctx))))
 
@@ -53,20 +56,70 @@
     (if (list? arg)
       (car (canonicalize-args (list (last arg))
                               (append props (reverse (cdr (reverse (cdr arg)))))))
-      (append (list '!) props (list arg)))))
+      (if (null? props)
+        arg
+        (append (list '!) props (list arg))))))
 
 (define (compile-program prog #:name name)
   (match-define (list 'FPCore (list args ...) props ... body) prog)
   (define-values (_ properties) (parse-properties props))
-  (define ctx (make-immutable-hash properties))
-  `(FPCore ,(canonicalize-args args props) ,(expr->canon body ctx)))
+  (define propagate-properties (for/list ([prop properties]
+                                          #:when (set-member? (*to-propagate*) (car prop)))
+                                 prop))
+  (define to-canonicalize-properties (for/list ([prop properties]
+                                             #:when (set-member? (*to-canonicalize*) (car prop)))
+                                       prop))
+  (define canonicalized-properties (for/list ([prop to-canonicalize-properties])
+                                     (if (list? prop)
+                                       (cons (car prop) (expr->canon (cadr prop) (make-immutable-hash propagate-properties)))
+                                       (cons (car prop) (cdr prop)))))
+  (define top-level-properties (append canonicalized-properties (set-subtract
+                                                                  properties
+                                                                  propagate-properties
+                                                                  to-canonicalize-properties)))
+  (define propagate-ctx (make-immutable-hash propagate-properties))
+  `(FPCore
+     ,(canonicalize-args args (flatten-context propagate-ctx))
+     ,@(flatten-context (make-immutable-hash top-level-properties))
+     ,(expr->canon body propagate-ctx)))
 
 (module+ main
   (require racket/cmdline)
 
-  (command-line
-   #:program "canonicalizer.rkt"
-   #:args ()
-   (port-count-lines! (current-input-port))
-   (for ([expr (in-port (curry read-fpcore "stdin"))] [n (in-naturals)])
-     (pretty-write (compile-program expr #:name (format "ex~a" n))))))
+  (define (custom-command-line command-line-args)
+    (parse-command-line "canonicalizer" command-line-args
+      `((once-each
+         [("-p" "--propagate")
+          ,(lambda (lf . args) (begin
+                                 (define other-arg-index (index-where args (λ (x) (eq? (string-ref x 0) #\-))))
+                                 (if other-arg-index
+                                   (let ([these-args (map (λ (x) (string->symbol (string-append ":" x))) (take args other-arg-index))]
+                                         [other-args (list-tail args other-arg-index)])
+                                     (*to-propagate* these-args)
+                                     (custom-command-line other-args))
+                                   (*to-propagate* (map (λ (x) (string->symbol (string-append ":" x))) args)))))
+          ("Properties for the canonicalizer to propagate (defaults to precision, round, and math-library)"
+           "arg ...")]
+         [("-c" "--canonicalize")
+          ,(lambda (lf . args) (begin
+                                 (define other-arg-index (index-where args (λ (x) (eq? (string-ref x 0) #\-))))
+                                 (if other-arg-index
+                                   (let ([these-args (map (λ (x) (string->symbol (string-append ":" x))) (take args other-arg-index))]
+                                         [other-args (list-tail args other-arg-index)])
+                                     (*to-canonicalize* these-args)
+                                     (custom-command-line other-args))
+                                   (*to-canonicalize* (map (λ (x) (string->symbol (string-append ":" x))) args)))))
+          ("Properties for the canonicalizer to canonicalize (defaults to pre and spec)"
+           "arg ...")]))
+      (lambda (flag-accum) null)
+        null))
+
+  ;; Assign the output to _ so it doesn't print
+  (define _ (custom-command-line (current-command-line-arguments)))
+  (unless (empty? (set-intersect (*to-propagate*) (*to-canonicalize*)))
+    (println "Error: a property cannot be both propagated and canonicalized")
+    (custom-command-line #("-h")))
+
+  (port-count-lines! (current-input-port))
+  (for ([expr (in-port (curry read-fpcore "stdin"))] [n (in-naturals)])
+    (pretty-write (compile-program expr #:name (format "ex~a" n)))))

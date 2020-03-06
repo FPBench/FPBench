42a03300ae110457ca4d2c4a27315b8b34758b93
Brett Saiki <bksaiki@gmail.com>
2020-03-06 10:57:30 -0800

    Wolfram let* and while* unsupported

--- src/core2wls.rkt
+++ src/core2wls.rkt
@@ -4,7 +4,7 @@
 (provide core->wls wls-supported number->wls)
 
 (define wls-supported (supported-list
-  (invert-op-list '())
+  (invert-op-list '(let* while*))
   (invert-const-list '())
   '(binary32 binary64)))
 

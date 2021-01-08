633fd4893d4c8450c5ce5c47acde674cb29a0d84
Alexey Solovyev <solovyev.alexey@gmail.com>
2021-01-07 20:47:41 -0800

    test-core2fptaylor: round computed bounds

--- infra/test-core2fptaylor.rkt
+++ infra/test-core2fptaylor.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require math/flonum)
+(require math/flonum racket/extflonum math/bigfloat)
 (require "test-common.rkt" "../src/common.rkt" "../src/core2fptaylor.rkt" "../src/range-analysis.rkt")
 
 (define (string->double string)
@@ -14,6 +14,21 @@
   (call-with-output-file test-file #:exists 'replace
     (lambda (port) (fprintf port "~a\n" (core->fptaylor prog "f"))))
   test-file)
+
+; From test-core2c
+(define (round-result type out)
+  (define out*
+    (match out
+      ["nan" "+nan.0"]
+      ["-nan" "+nan.0"]
+      ["inf" "+inf.0"]
+      ["-inf" "-inf.0"]
+      [x x]))
+  (match type
+    ['binary80 (parameterize ([bf-precision 64]) (real->extfl (bigfloat->real (bf out*))))]
+    ['binary64 (real->double-flonum (string->number out*))]
+    ['binary32 (real->single-flonum (string->number out*))]
+    ['integer  (string->number out*)]))
 
 (define (run<-fptaylor exec-name ctx type number)
   (define out (run-with-time-limit "fptaylor" 
@@ -45,8 +60,8 @@
   (if timeout?
     (cons 'timeout "timeout")
     (cons 
-      (cons (string->double (car out*))   
-            (string->double (cdr out*)))
+      (cons (round-result type (car out*))   
+            (round-result type (cdr out*)))
       (format "[~a, ~a]" (car out*) (cdr out*)))))
 
 ; =*

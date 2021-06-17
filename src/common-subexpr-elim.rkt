9c3eff21d0f9494e44e143286fe42f981f20686e
Brett Saiki <bksaiki@gmail.com>
2021-06-17 14:00:46 -0700

    fuse lets, still broken

--- src/common-subexpr-elim.rkt
+++ src/common-subexpr-elim.rkt
@@ -3,21 +3,21 @@
 ;; TODO: Once the let unroller is implemented we can eliminate more subexpressions.
 ;; Right now we don't look inside any let or while statements for eliminating, but once
 ;; the unroller is implemented, we can just expand everything for MAXIMUM elimination.
-(require "common.rkt" "fpcore-reader.rkt")
+(require "common.rkt" "fpcore-reader.rkt" "fpcore-visitor.rkt")
 (module+ test (require rackunit))
 
 (provide core-common-subexpr-elim)
 
 (define *names* (make-parameter (mutable-set)))
 
-;; old gen-sym
+;;;;;; old gensym
+
 (define (gensym name)
   (define prefixed
     (filter (λ (x) (string-prefix? (~a x) (~a name))) (set->list (*names*))))
   (define options
-    (cons name
-      (for/list ([_ prefixed] [i (in-naturals)])
-        (string->symbol (format "~a~a" name i)))))
+    (for/list ([_ (cons name prefixed)] [i (in-naturals)])
+      (string->symbol (format "~a~a" name i))))
   (define name*
     (car (set-subtract options prefixed)))
   (set-add! (*names*) name*)
@@ -26,13 +26,34 @@
 (define (add-name! name)
   (set-add! (*names*) name))
 
+;;;;;; fuse let expressions
+
+(define (visit-let/fuse visitor vars vals body #:ctx [ctx '()])
+  (match body
+   [`(,(or 'let 'let*) ([,vars2 ,vals2] ...) ,body2)
+    (define comb-vars (append vars vars2))
+    (if (check-duplicates comb-vars) ; don't fuse with duplicates
+        `(let (,@(for/list ([var vars] [val vals]) (list var (visit/ctx visitor val ctx))))
+              ,(visit/ctx visitor body ctx))
+        (visit/ctx visitor
+                   `(let* (,@(map list comb-vars (append vals vals2))) ,body2)
+                   ctx))]
+   [else
+    `(let (,@(for/list ([var vars] [val vals]) (list var (visit/ctx visitor val ctx))))
+          ,(visit/ctx visitor body ctx))]))
+
+(define/transform-expr (fuse-let expr)
+  [visit-let visit-let/fuse])
+
+;;;;;; main cse
+
 (define (common-subexpr-elim vars expr)
   (define exprhash
     (make-hash
       (for/list ([var vars] [i (in-naturals)])
         (cons var i))))
   (define common (mutable-set))
-  (define exprs (map list vars))
+  (define exprs '())
   (define exprc (length vars))
   (define varc exprc)
 
@@ -56,17 +77,18 @@
         (set! exprs (cons mgd exprs)))]))
 
   (define root (munge expr))
-  (define exprvec (list->vector (reverse exprs)))
+  (define exprvec (list->vector (append (map list vars) (reverse exprs))))
 
   ; scan dependent indices
-  (define (dependent-indices idx)
+  (define (dependent-indices root)
     (filter (curryr >= varc)
       (remove-duplicates
-        (let loop ([idx idx])
-          (define expr (vector-ref exprvec idx))
-          (match expr
-           [(list op args ...) (apply append (map loop args))]
-           [else (list expr)])))))
+        (let loop ([idx root])
+          (match-define (list op args ...) (vector-ref exprvec idx))
+          (define idxs (apply append (map loop args)))
+          (if (and (not (= idx root)) (set-member? common idx))
+              (cons idx idxs)
+              idxs)))))
 
   (define common-depends  ; idx -> list of dependent indices
     (make-hash
@@ -83,15 +105,16 @@
   ; reconstruct expression
   (define (gen-let-bindings)
     (define idxs (set->list bindable)) ; copy
+    (define names (build-list (set-count bindable) (λ (_) (gensym 't))))
     (set-clear! bindable)
-    (for/list ([idx idxs])
-      (define name (gensym 't))
-      (begin0 (list name (reconstruct idx))
+    (begin0 (for/list ([name names] [idx idxs]) ; descend
+              (list name (reconstruct idx)))
+      (for ([name names] [idx idxs])            ; then replace exprs
         (vector-set! exprvec idx (list name)))))
 
   (define (update-common! idx)
     (for ([(k v) (in-hash common-depends)])
-      (hash-update! common-depends k (curry remove)))
+      (hash-update! common-depends k (curry remove idx)))
     (for ([idx (in-set common)])
       (when (null? (hash-ref common-depends idx))
         (set-remove! common idx)
@@ -110,9 +133,9 @@
                [else 
                 (if (list? (car expr)) expr (car expr))])
         (update-common! idx))))
-
   (reconstruct root))
 
+;;;;;;; top-level
 
 (define (core-common-subexpr-elim core)
   (*names* (mutable-set))
@@ -137,32 +160,28 @@
      (printf "~a\n" (core-common-subexpr-elim expr)))))
 
 (module+ test
-  (check-equal?
-    (core-common-subexpr-elim '(FPCore (a) a))
-    '(FPCore (a) a))
+  (define (check-cse in out)
+    (check-equal? (core-common-subexpr-elim in) out))
 
-  (check-equal?
-    (core-common-subexpr-elim '(FPCore (a) (+ (+ a a) (+ a a))))
-    '(FPCore (a) (let ((t (+ a a))) (+ t t))))
+  (check-cse '(FPCore (a) a)
+             '(FPCore (a) a))
 
-  (check-equal?
-    (core-common-subexpr-elim '(FPCore (a x) (+
-                                               (- (+ a x) a)
-                                               (- (+ a x) a))))
-    '(FPCore (a x) (let ((t (- (+ x a) x))) (+ t t))))
+  (check-cse '(FPCore (a) (+ (+ a a) (+ a a)))
+             '(FPCore (a) (let ((t (+ a a))) (+ t t))))
+
+  (check-cse '(FPCore (a x) (+ (- (+ a x) a)
+                               (- (+ a x) a)))
+             '(FPCore (a x) (let ((t (- (+ x a) x))) (+ t t))))
   
-  (check-equal?
-    (core-common-subexpr-elim '(FPCore (a) (let ((j0 (+ a a))) j0)))
-    '(FPCore (a) (let ((j0 (+ a a))) j0)))
+  (check-cse '(FPCore (a) (let ((j0 (+ a a))) j0))
+             '(FPCore (a) (let ((j0 (+ a a))) j0)))
 
-  (check-equal?
-    (core-common-subexpr-elim '(FPCore (a) (let ((j0 (+ a a))) (+ (+ a a) j0))))
-    '(FPCore (a) (let ((j0 (+ a a))) (+ (+ a a) j0))))
+  (check-cse '(FPCore (a) (let ((j0 (+ a a))) (+ (+ a a) j0)))
+             '(FPCore (a) (let ((j0 (+ a a))) (+ (+ a a) j0))))
 
-  (check-equal?
-    (core-common-subexpr-elim '(FPCore (a) (let ((i0 (- a a))) (- (+ (+ a a) (+ a a)) i0))))
-    '(FPCore (a) (let ((t (+ a a))) (let ((i0 (- a a))) (- (+ t t) i0)))))
+  (check-cse '(FPCore (a) (let ((i0 (- a a))) (- (+ (+ a a) (+ a a)) i0)))
+             '(FPCore (a) (let* ((t (+ a a)) (i0 (- a a))) (- (+ t t) i0))))
 
-  (check-equal?
-    (core-common-subexpr-elim '(FPCore (a) (let ((x (- a a))) (let ((x (+ a a))) x))))
-    '(FPCore (a) (let ((x (- a a))) (let ((x (+ a a))) x)))))
+  (check-cse '(FPCore (a) (let ((x (- a a))) (let ((x (+ a a))) x)))
+             '(FPCore (a) (let ((x (- a a))) (let ((x (+ a a))) x))))
+)

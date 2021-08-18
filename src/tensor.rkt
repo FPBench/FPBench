cf0d95e9bd2ec51f93e65aafc28eecfca26ed8a1
Brett Saiki <bksaiki@gmail.com>
2021-08-18 15:00:01 -0700

    Clean up contracts in interpreter and tensor.rkt

--- src/tensor.rkt
+++ src/tensor.rkt
@@ -1,7 +1,14 @@
 #lang racket
 
 (require "common.rkt")
-(provide tensor? tensor-ref tensor-dim tensor-size tabulate->tensor)
+(provide
+  (contract-out
+   [tensor? (-> any/c boolean?)]
+   [tensor-ref (-> tensor? integer? ... integer? any/c)]
+   [tensor-dim (-> tensor? integer?)]
+   [tensor-size (-> tensor? integer? integer?)]
+   [tabulate->tensor (-> (listof integer?) (listof any/c) tensor?)]))
+
 
 (define (tensor-val? x)
   (match x
@@ -12,33 +19,32 @@
    [_ #f]))
 
 (define (tensor? x)
-  (cond
-    [(list? x) (andmap tensor-val? x)]
-    [else #f]))
+  (and (list? x) (andmap tensor-val? x)))
 
 (define (tensor-ref x n . rest)
-  (-> tensor? integer? ... integer? any/c)
-  (cond [(empty? rest) (list-ref x n)]
-        [else (apply (curry tensor-ref (list-ref x n)) rest)]))
+  (if (empty? rest)
+      (list-ref x n)
+      (apply (curry tensor-ref (list-ref x n)) rest)))
 
 (define (tensor-dim x)
-  (-> tensor? integer? ... integer? any/c)
   (let loop ([t x])
-    (cond [(for/and ([i t]) (tensor? i)) (add1 (apply min (map loop t)))]
-          [else 1])))
+    (if (andmap tensor? t)
+        (+ (apply min (map loop t)) 1)
+        1)))
 
 (define (tensor-size x n)
-  (-> tensor? integer? integer?)
-  (length (list-ref x n)))
+  (let loop ([x x] [n n])
+    (if (zero? n)
+        (length x)
+        (loop (list-ref x 0) (- n 1)))))
 
 (define (tabulate->tensor sizes vals)
-  (-> (listof integer?) (listof any/c) tensor?)
   (unless (= (apply * sizes) (length vals))
     (error 'tabulate->tensor "Not enough values to create a 'rectangular' tensor"))
-  (let inner ([sizes* sizes] [vals* vals])
-    (cond
-      [(= (length sizes*) 1) vals*]
-      [else 
+  (let loop ([sizes* sizes] [vals* vals])
+    (if (= (length sizes*) 1)
+        vals*
         (let ([div (/ (length vals*) (first sizes*))])
           (for/list ([i (in-range (first sizes*))])
-            (inner (drop sizes* 1) (take (drop vals* (* i div)) div))))])))
+            (loop (drop sizes* 1)
+                  (take (drop vals* (* i div)) div)))))))

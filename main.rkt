c8b48de1d686a52006b7b87efad16a0172e0ad62
varun10p <varun10p@cs.washington.edu>
2024-04-10 16:35:54 -0700

    Remove imports

--- main.rkt
+++ main.rkt
@@ -1,49 +1,5 @@
 #lang racket
 
-(require 
-  "transform.rkt"
-  "evaluate.rkt"
-  "export.rkt"
-  "toolserver.rkt"
-  "infra/filter.rkt")
-
-(require
- "src/canonicalizer.rkt"
- "src/common.rkt"
- "src/common-subexpr-elim.rkt"
- "src/compilers.rkt"
- "src/core2c.rkt"
- "src/core2cakeml.rkt"
- "src/core2fptaylor.rkt"
- "src/core2fortran03.rkt"
- "src/core2gappa.rkt"
- "src/core2go.rkt"
- "src/core2haskell.rkt"
- "src/core2java.rkt"
- "src/core2js.rkt"
- "src/core2julia.rkt"
- "src/core2matlab.rkt"
- "src/core2ocaml.rkt"
- "src/core2python.rkt"
- "src/core2scala.rkt"
- "src/core2smtlib2.rkt"
- "src/core2sollya.rkt"
- "src/core2tex.rkt"
- "src/core2vivado.rkt"
- "src/core2wls.rkt"
- "src/fpcore-extra.rkt"
- "src/fpcore-checker.rkt"
- "src/fpcore-interpreter.rkt"
- "src/fpcore-reader.rkt"
- "src/imperative.rkt"
- "src/multi-command-line.rkt"
- "src/range-analysis.rkt"
- "src/supported.rkt"
-
- "infra/core2json.rkt"
- "infra/filter.rkt"
- "infra/gen-expr.rkt")
-
 (provide
  (all-from-out
   "src/canonicalizer.rkt"
